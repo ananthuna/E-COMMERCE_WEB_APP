de0fbe6fe8a1c8492fa084b63c496ed2617a7d7a
ananthuna <ananthu4uuu@gmail.com>
2023-06-29 13:25:11 +0530

    build added

---
+++ server/build/precache-manifest.650c668cda1317d2a3f1ad9b3778fac2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8ec82f4f20ae1463801",
+    "url": "/static/css/main.11b347bb.chunk.css"
+  },
+  {
+    "revision": "b8ec82f4f20ae1463801",
+    "url": "/static/js/main.b8ec82f4.chunk.js"
+  },
+  {
+    "revision": "00208d8770b68ab6b82c",
+    "url": "/static/js/1.00208d87.chunk.js"
+  },
+  {
+    "revision": "69dfc23c71e831e7ef2c",
+    "url": "/static/js/2.69dfc23c.chunk.js"
+  },
+  {
+    "revision": "85cec025a53deb3facdc",
+    "url": "/static/js/3.85cec025.chunk.js"
+  },
+  {
+    "revision": "1b3ea013ec1e22af58b7",
+    "url": "/static/js/4.1b3ea013.chunk.js"
+  },
+  {
+    "revision": "8d94e8de12fc4bb164d6",
+    "url": "/static/css/5.0b1ed4be.chunk.css"
+  },
+  {
+    "revision": "8d94e8de12fc4bb164d6",
+    "url": "/static/js/5.8d94e8de.chunk.js"
+  },
+  {
+    "revision": "967a1cf197fca1f7e9cb",
+    "url": "/static/css/6.df76d726.chunk.css"
+  },
+  {
+    "revision": "967a1cf197fca1f7e9cb",
+    "url": "/static/js/6.967a1cf1.chunk.js"
+  },
+  {
+    "revision": "a06494281b299f839033",
+    "url": "/static/js/runtime~main.a0649428.js"
+  },
+  {
+    "revision": "050a0651fc28694e0fceb68ada0d125c",
+    "url": "/index.html"
+  }
+];
--- server/build/service-worker.js
+++ server/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be9247e2d17a7cf24f8afd23b1488289.js"
+  "/precache-manifest.650c668cda1317d2a3f1ad9b3778fac2.js"
 );
 
 workbox.clientsClaim();
